44cca2053e301d9f9d823989a7f75d54b6453a60
dennismacc <dennis.maccarthy@gmail.com>
2022-06-22 12:26:26 -0700

    write engineer, intern, and manager js files

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,7 +1,27 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-  // TODO - Write Engineer class so it satisfies the test cases when running `npm run test`
-}
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.github = github;
+  }
+  getName() {
+    return this.name;
+  }
+  getId() {
+    return this.id;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getGithub() {
+    return this.github;
+  }
+  getRole() {
+    return "Engineer";
+  }
+};
 
 module.exports = Engineer;
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,7 +1,27 @@
 const Employee = require("./Employee");
 
 class Intern extends Employee {
-  // TODO - Write Intern class so it satisfies the test cases when running `npm run test`
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.school = school;
+  }
+  getName() {
+    return this.name;
+  }
+  getId() {
+    return this.id;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getSchool() {
+    return this.school;
+  }
+  getRole() {
+    return "Intern";
+  }
 }
 
 module.exports = Intern;
