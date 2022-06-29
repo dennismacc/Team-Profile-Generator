c7c226e09093334d85f0288ae08ba022263f59dd
dennismacc <dennis.maccarthy@gmail.com>
2022-06-29 12:54:00 -0700

    write manager prompt questions

---
+++ src/questions.js
@@ -0,0 +1,22 @@
+const managerQuestions = [
+    { 
+       type: 'input',
+       name: 'managerName',
+       message: 'What is the managers name?',
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "What is the team manager's id?"
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the team manager's email?"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the team manager's office number?"
+    }
+]
